59e8046e2f2afadf948a60083bc12884d5b06b24
Kagamimochi <192094637+Kagami-omochi@users.noreply.github.com>
2026-06-08 12:39:40 +0900

    Create hyprland.lua for Hyprland configuration
    Added initial configuration for Hyprland with required modules.

---
+++ home/.config/hypr/hyprland.lua
@@ -0,0 +1,15 @@
+-- Refer to the wiki for more information.
+-- https://wiki.hypr.land/Configuring/Start/
+
+-- Please note not all available settings / options are set here.
+-- For a full list, see the wiki
+
+require("conf/autostart")
+require("conf/env")
+require("conf/keybinds")
+require("conf/windowrule")
+require("conf/env")
+require("conf/lookandfeel")
+require("conf/plugin")
+require("conf/monitor")
+require("private")
