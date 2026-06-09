45a02714d691f3b85dec8780be0ca1151373cf27
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-06-09 10:33:09 +0900

    Refactor input configuration in input.lua

--- home/.config/hypr/conf/input.lua
+++ home/.config/hypr/conf/input.lua
@@ -1,7 +1,6 @@
 hl.config({
     input = {
         kb_layout  = "us",
-        follow_mouse = 1,
         sensitivity = -0.3, -- -1.0 - 1.0, 0 means no modification.
         accel_profile = "flat"
     },
