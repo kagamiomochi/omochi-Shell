6a975f27e73901dc2be381c9caef8ad27abed99a
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-06-15 10:18:17 +0900

    Refactor plugin configuration for hyprbars and cursors

--- home/.config/hypr/conf/plugin.lua
+++ home/.config/hypr/conf/plugin.lua
@@ -1,19 +1,33 @@
-###############
-### PLUGINS ###
-###############
+-- https://wiki.hypr.land/Plugins/Using-Plugins/
 
-plugin {
-    hyprbars {
-        bar_height = 32
-        bar_blur = true
-        bar_button_padding = 15
-        bar_part_of_window = true
-        bar_precedence_over_border = true
-        hyprbars-button = rgba(00000000), 18, ✕, hyprctl dispatch killactive
-        hyprbars-button = rgba(00000000), 18, ❏, hyprctl dispatch fullscreen 1
-        on_double_click = hyprctl dispatch fullscreen 1
-    }
-}
+hl.config({
+    plugin = {
+        hyprbars = {
+            bar_height = 32,
+            bar_blur = true,
+            bar_button_padding = 15,
+            bar_part_of_window = true,
+            bar_precedence_over_border = true,
+            on_double_click = "hyprctl dispatch fullscreen 1",
+        },
+    },
+})
+
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgba(00000000)",
+    fg_color = "rgb(ffffff)",
+    size =18,
+    icon = "✕",
+    action = "hyprctl dispatch killactive",
+})
+
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgba(00000000)",
+    fg_color = "rgb(ffffff)",
+    size = 18,
+    icon = "❏",
+    action = "hyprctl dispatch fullscreen 1",
+})
 
 plugin:dynamic-cursors {
 
