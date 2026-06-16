9c5fed49fac06f1afca00b05ec84d170aadeb713
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-06-16 18:27:02 +0900

    Refactor hyprbars setup into a function

--- home/.config/hypr/conf/plugin.lua
+++ home/.config/hypr/conf/plugin.lua
@@ -1,30 +1,40 @@
 -- https://wiki.hypr.land/Plugins/Using-Plugins/
 
-hl.config({
-    plugin = {
-        hyprbars = {
-            bar_height = 32,
-            bar_blur = true,
-            bar_button_padding = 15,
-            bar_part_of_window = true,
-            bar_precedence_over_border = true,
-            on_double_click = "hyprctl dispatch fullscreen 1",
-        },
-    },
-})
+local M = {}
 
-hl.plugin.hyprbars.add_button({
-    bg_color = "rgba(00000000)",
-    fg_color = "rgb(ffffff)",
-    size = 18,
-    icon = "✕",
-    action = "hyprctl dispatch killactive",
-})
+function M.setup_hyprbars()
+    if hl.plugin.hyprbars ~= nil then
+        
+        hl.config({
+            plugin = {
+                hyprbars = {
+                    bar_height = 32,
+                    bar_blur = true,
+                    bar_button_padding = 15,
+                    bar_part_of_window = true,
+                    bar_precedence_over_border = true,
+                    on_double_click = "hyprctl dispatch fullscreen 1",
+                },
+            },
+        })
 
-hl.plugin.hyprbars.add_button({
-    bg_color = "rgba(00000000)",
-    fg_color = "rgb(ffffff)",
-    size = 18,
-    icon = "❏",
-    action = "hyprctl dispatch fullscreen 1",
-})
+        hl.plugin.hyprbars.add_button({
+            bg_color = "rgba(00000000)",
+            fg_color = "rgb(ffffff)",
+            size = 18,
+            icon = "✕",
+            action = "hyprctl dispatch killactive",
+        })
+
+        hl.plugin.hyprbars.add_button({
+            bg_color = "rgba(00000000)",
+            fg_color = "rgb(ffffff)",
+            size = 18,
+            icon = "❏",
+            action = "hyprctl dispatch fullscreen 1",
+        })
+
+    end
+end
+
+M.setup_hyprbars()
