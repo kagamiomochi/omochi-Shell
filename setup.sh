0e85a8fef5344f369584850830fa7092c7dc7221
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-07-10 10:01:37 +0900

    Refactor package installation command in setup.sh

--- setup.sh
+++ setup.sh
@@ -40,7 +40,8 @@ if ! command -v paru &> /dev/null; then
 fi
 
 echo "Install prerequisite packages..."
-sudo pacman -S hyprland \
+sudo pacman -S \
+hyprland \
 --needed --noconfirm
 
 # Load packages from package list and install them
