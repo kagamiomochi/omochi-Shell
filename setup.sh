c059cc83a0c5a1e0ae47edc92f8cb1c949a31f0f
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-08-06 12:44:54 +0900

    Comment out dynamic-cursors plugin enable command
    Comment out the hyprpm enable command for dynamic-cursors.

--- setup.sh
+++ setup.sh
@@ -47,6 +47,7 @@ grep -Ev '^\s*($|#)' "$PACKAGES_FILE" | xargs -r paru -S --needed --noconfirm
 #hyprpm update
 #yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
 #yes | hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
+#hyprpm enable dynamic-cursors
 
 # Linking dotfiles 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
