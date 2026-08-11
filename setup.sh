53e1ce070a0a6b035e8a3b82f5c37b46873cf154
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-08-11 15:14:44 +0900

    add firewall rule

--- setup.sh
+++ setup.sh
@@ -91,8 +91,18 @@ sudo systemctl enable --now bluetooth
 sudo systemctl enable --now keyd
 
 # firewall
-sudo ufw allow 1714:1764/tcp
+sudo ufw allow 1714:1764/tcp # KDE Connect
 sudo ufw allow 1714:1764/udp
+
+sudo ufw allow 80/tcp # Steam
+sudo ufw allow 443/tcp
+sudo ufw allow 27015:27050/tcp
+sudo ufw allow 27000:27250/udp
+sudo ufw allow 27031:27036/udp
+sudo ufw allow 27036/tcp
+sudo ufw allow 4380/udp
+sudo ufw allow 3478/udp
+sudo ufw allow 4379/udp
 
 # Theme setting
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
