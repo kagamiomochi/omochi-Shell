930573d95dde60c69209f2c134c8e5c87e828c50
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-03 21:16:18 +0900

    QT_STYLE_OVERRIDE=kvantum を追加

--- home/.config/hypr/conf/env.lua
+++ home/.config/hypr/conf/env.lua
@@ -15,6 +15,7 @@ hl.env("CLUTTER_BACKEND", "wayland")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_STYLE_OVERRIDE", "kvantum")
 
 hl.env("HYPRCURSOR_THEME", "BreezeX-Dark")
 hl.env("HYPRCURSOR_SIZE", "24")
