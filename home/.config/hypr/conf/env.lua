a84a0245e00b17518c6ab95d31a4ce443150d625
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-09 08:53:24 +0900

    fix: Added GTK_IM_MODULE to enable Japanese input to Electron software.

--- home/.config/hypr/conf/env.lua
+++ home/.config/hypr/conf/env.lua
@@ -23,6 +23,7 @@ hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("XCURSOR_SIZE", "24")
 
 hl.env("QT_IM_MODULE", "fcitx")
+hl.env("GTK_IM_MODULE", "fcitx")
 hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("SDL_IM_MODULE", "fcitx")
 
