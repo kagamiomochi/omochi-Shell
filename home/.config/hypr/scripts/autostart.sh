47a42bfbc4dd2637398426db92b91790837245da
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-09 08:55:29 +0900

    feat: Start GNOME Keyring daemon at startup

--- home/.config/hypr/scripts/autostart.sh
+++ home/.config/hypr/scripts/autostart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 dbus-update-activation-environment --systemd --all
+gnome-keyring-daemon --start --components=secrets,pkcs11,ssh
 hyprctl setcursor "Bibata-Modern-Classic" 24
 awww-daemon &
 
