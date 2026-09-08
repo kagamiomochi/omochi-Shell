82bb53ffe46512eb6a984e7ac9b8a0e9fc698374
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-09 08:54:11 +0900

    fix: It was too stupid

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -2,10 +2,10 @@
 
 
 ----- If you want to add your own configuration without overriding the shell configuration, please write your code above this line. -----
-hl.exec_cmd("hyprpm update")
 hl.on("hyprland.start", function ()
-  hl.exec_cmd("chmod +x ~/.config/hypr/scripts/autostart.sh; ~/.config/hypr/scripts/autostart.sh")
-  hl.exec_cmd([[
+    hl.exec_cmd("hyprpm reload")
+    hl.exec_cmd("chmod +x ~/.config/hypr/scripts/autostart.sh; ~/.config/hypr/scripts/autostart.sh")
+    hl.exec_cmd([[
         FLAG="$HOME/.local/state/omochi-shell/.setup_done"
         if [ ! -f "$FLAG" ]; then
             mkdir -p "$(dirname "$FLAG")"
