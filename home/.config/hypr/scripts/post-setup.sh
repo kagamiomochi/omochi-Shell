5f9c10a8fdc93ea8192727f073fa3afce2245994
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-10 18:18:19 +0900

    fix: Move Hyprland Plugin updates etc. here

--- home/.config/hypr/scripts/post-setup.sh
+++ home/.config/hypr/scripts/post-setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 touch ~/.config/hypr/private.lua
+
+# Install Hyprland plugins
+hyprpm update
+yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
+yes | hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
+hyprpm enable dynamic-cursors
