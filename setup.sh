1e401c6fdd05657402350835ee7a4c9622c55b9c
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-10 18:16:18 +0900

    I don't really understand anymore

--- setup.sh
+++ setup.sh
@@ -43,12 +43,6 @@ hyprland \
 # Load packages from package list and install them
 grep -Ev '^\s*($|#)' "$PACKAGES_FILE" | xargs -r paru -S --needed --noconfirm
 
-# Install Hyprland plugins
-#hyprpm update
-#yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
-#yes | hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
-#hyprpm enable dynamic-cursors
-
 # Linking dotfiles 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 HOME_DIR="$DOTFILES_DIR/home"
