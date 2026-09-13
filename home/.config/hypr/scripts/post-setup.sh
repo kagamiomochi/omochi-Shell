d8f370c35d5dc0482e997863615dff4fd6c4e6fe
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-13 21:39:55 +0900

    post-setup.sh の更新

--- home/.config/hypr/scripts/post-setup.sh
+++ home/.config/hypr/scripts/post-setup.sh
@@ -15,5 +15,5 @@ hyprpm enable dynamic-cursors
 
 hyprpm reload
 
-mkdir -p "$(dirname "$HOME/.local/state/omochi-shell/.setup_done")"
+mkdir -p ""$HOME/.local/state/omochi-shell/.setup_done"
 touch "$HOME/.local/state/omochi-shell/.setup_done"
