9317b7ac4b062985b24880a6229030d3878d9cb5
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-14 21:28:41 +0900

    fix: Modified so that everything can be passed with NOPASSWD

--- setup.sh
+++ setup.sh
@@ -95,7 +95,7 @@ sudo systemctl enable greetd
 SUDOERS_FILE="/etc/sudoers.d/post-setup-tmp"
 
 sudo tee "$SUDOERS_FILE" > /dev/null <<EOF
-$USER ALL=(ALL) NOPASSWD: "$DOTFILES_DIR/post-setup.sh", /usr/bin/rm -f $SUDOERS_FILE
+$USER ALL=(ALL) NOPASSWD: ALL
 EOF
 sudo chmod 0440 "$SUDOERS_FILE"
 sudo visudo -c -f "$SUDOERS_FILE"
