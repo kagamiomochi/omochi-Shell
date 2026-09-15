0e1dfaeec5061674f50f4b0f5042f9a91ca21425
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-15 10:01:52 +0900

    Fix path to post-setup.sh in hyprland.lua

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -7,7 +7,7 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("chmod +x ~/.config/hypr/scripts/autostart.sh; ~/.config/hypr/scripts/autostart.sh")
     hl.exec_cmd([[
         if [ ! -f "$HOME/.local/state/omochi-shell/.setup_done" ]; then
-            ~/.config/hypr/scripts/post-setup.sh
+            ~/omochi-Shell/post-setup.sh
         fi
     ]])
 end)
