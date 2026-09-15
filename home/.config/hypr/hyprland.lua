314b4b2290487908bb95062028770fb257c155c1
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-15 10:48:48 +0900

    Update script path for post-setup.sh

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -7,7 +7,7 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("chmod +x ~/.config/hypr/scripts/autostart.sh; ~/.config/hypr/scripts/autostart.sh")
     hl.exec_cmd([[
         if [ ! -f "$HOME/.local/state/omochi-shell/.setup_done" ]; then
-            ~/omochi-Shell/post-setup.sh
+            "$DOTFILES_DIR/post-setup.sh"
         fi
     ]])
 end)
