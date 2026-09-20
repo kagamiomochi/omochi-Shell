e5471062e1843a6d475f1d8ba432fd99026dcfd6
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-20 22:07:16 +0900

    fix: symlink

--- setup.sh
+++ setup.sh
@@ -205,7 +205,7 @@ step_system_symlinks() {
     local DOTFILES_DIR="$1"
     sudo ln -sfn "$DOTFILES_DIR/system/etc/keyd/default.conf"                    /etc/keyd/default.conf
     sudo ln -sfn "$DOTFILES_DIR/system/etc/pam.d/hyprlock"                       /etc/pam.d/hyprlock
-    sudo ln -sfn "$DOTFILES_DIR/system/etc/systemd/system/automount-all.service" /etc/systemd/system/automount-all.service
+    sudo cp "$DOTFILES_DIR/system/etc/systemd/system/automount-all.service"      /etc/systemd/system/
     
     sudo mkdir -p /usr/local/bin
     sed "s/__USERNAME__/${USER}/g" "$DOTFILES_DIR/system/usr/local/bin/automount-all.sh.template" | sudo tee /usr/local/bin/automount-all.sh > /dev/null
