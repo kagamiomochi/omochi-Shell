ed6cddd0a4124f854a01338d8e03c9985d40249d
kagamiomochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-25 21:26:21 +0900

    fix: The environment variable was not expanded.

--- home/.config/hypr/conf/env.lua
+++ home/.config/hypr/conf/env.lua
@@ -28,4 +28,4 @@ hl.env("XMODIFIERS", "@im=fcitx")
 hl.env("SDL_IM_MODULE", "fcitx")
 
 hl.env("__GL_SHADER_DISK_CACHE_SIZE", "12000000000")
-hl.env("XDG_DATA_DIRS", "$HOME/.local/share:/usr/local/share:/usr/share")
+hl.env("XDG_DATA_DIRS", os.getenv("HOME") .. "/.local/share:/usr/local/share:/usr/share")
